620651b6635383f8bb34ce5288ccda42ede5efc9
mohammadsaqib1997 <mohammadsaqib1997@gmail.com>
2017-04-29 21:15:59 +0500

    Some Changes

--- app.js
+++ app.js
@@ -19,33 +19,39 @@ app.set('view engine', 'jade');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/web', users);
 
 /*app.use(history({
-    index: "/"
-}));*/
+ index: "/"
+ }));*/
 app.use('/', index);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+
+    res.render('error');
+    /*if(err.status == 404){
+        res.render('pages/error_404');
+    }else{
+        res.render('pages/error_500');
+    }*/
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'MineAim | Dashboard' });
 });
 
+router.get('/login', function(req, res, next) {
+    res.render('pages/login', { title: 'MineAim | Login' });
+});
+
+router.get('/500', function(req, res, next) {
+    res.render('pages/error_500', { title: 'MineAim | 500 Error' });
+});
+
+router.get('/404', function(req, res, next) {
+    res.render('pages/error_404', { title: 'MineAim | 404 Page not found' });
+});
+
 module.exports = router;
